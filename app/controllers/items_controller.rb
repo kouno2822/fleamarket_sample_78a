91a249cc7a3631f431f1e2c346e55810170171cf
KeiMaeLabo <65949665+KeiMaeLabo@users.noreply.github.com>
2020-07-04 17:36:48 +0900

    mark up finished

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,8 +4,34 @@ class ItemsController < ApplicationController
   
   def new
     @item = Item.new
-    # @item.images.new
+    @item.images.new
     @category_parent_array = ["選択してください"]
     @category_parent_array = Category.where(ancestry: nil)
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+  # 親カテゴリーが選択された後に動くアクション
+  def get_category_children
+    @category_children = Category.find(params[:parent_id]).children
+  end
+
+  # 子カテゴリーが選択された後に動くアクション
+  def get_category_grandchildren
+    @category_grandchildren = Category.find(params[:child_id]).children
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:name, :brand, :explanation, :status, :sell_or_sold, :delivery_burden,
+       :delivery_area, :delivery_day, :price, :user_id, :category_id, images_attributes: [:image])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
   } 
 
   root 'items#index'
-  resources :items
+  resources :items do
+    collection do
+      get 'category/get_category_children', to: 'items#get_category_children', defaults: { format: 'json' }
+      get 'category/get_category_grandchildren', to: 'items#get_category_grandchildren', defaults: { format: 'json' }
+    end
+  end
 end
