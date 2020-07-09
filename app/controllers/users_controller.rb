3354a3021f6185f31f99d248f29747daaf66ebf7
KeiMaeLabo <65949665+KeiMaeLabo@users.noreply.github.com>
2020-07-09 17:02:36 +0900

    changed user_id to seller_id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def show
-    @sell_items = Item.where(sell_or_sold: '出品中').where(user_id: current_user.id).order(created_at: :desc)
+    @sell_items = Item.where(sell_or_sold: '出品中').where(seller_id: current_user.id).order(created_at: :desc)
   end
 
   def logout
