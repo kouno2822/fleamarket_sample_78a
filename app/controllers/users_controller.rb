c220dd2c5c3df31fad41b646b216631fa8a01724
KeiMaeLabo <65949665+KeiMaeLabo@users.noreply.github.com>
2020-07-14 13:48:23 +0900

    users/show display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
 
   def show
     @sell_items = Item.where(sell_or_sold: '出品中').where(seller_id: current_user.id).order(created_at: :desc)
+    @buy_items = Item.where(buyer_id: current_user.id).order(created_at: :desc)
   end
 
   def logout
