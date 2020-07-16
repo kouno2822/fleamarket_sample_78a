6ce5c3c5de705e1d7296685be79f38edc5100f4f
BouzuSuzuki <ryuno15s@icloud.com>
2020-07-16 11:35:02 +0900

    商品詳細のページの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :move_to_login, except: :index
+  before_action :move_to_login, except: [:index, :show, :destroy]
+  before_action :set_item, only: [:show, :edit, :destroy]
 
   def index
     @parents = Category.where(ancestry: nil)
@@ -21,7 +22,19 @@ class ItemsController < ApplicationController
       redirect_to "/items/new", flash: { error: @item.errors.full_messages }
     end
   end
-  
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+  end
+
+  def show
+    @parents = Category.where(ancestry: nil)
+    @item_category_grandchild = Category.find(@item.category_id)
+    @item_category_child = @item_category_grandchild.parent
+    @item_category_parent = @item_category_child.parent
+  end
+
   def move_to_login
     redirect_to "/users/sign_in", notice: 'ログインするとご利用いただけます。' unless user_signed_in?
   end
@@ -36,10 +49,16 @@ class ItemsController < ApplicationController
     @category_grandchildren = Category.find(params[:child_id]).children
   end
   
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   private
   
   def item_params
     params.require(:item).permit(:name, :brand, :explanation, :status, :sell_or_sold, :delivery_burden,
        :prefecture_id, :delivery_day, :price, :seller_id, :buyer_id, :category_id, images_attributes: [:image])
   end
+
+
 end
