e4ddbe68433efe9c6c9c6e6bdce211000073aa2b
KeiMaeLabo <65949665+KeiMaeLabo@users.noreply.github.com>
2020-07-17 12:05:13 +0900

    Revert "Merge pull request #36 from BouzuSuzuki/details"
    This reverts commit d24699cbaff2db7599b3543562350fb3cbeba257, reversing
    changes made to ab38188299bf5715ef45378eb14dd12545cd26ed.

---
+++ app/assets/javascripts/payjp.js
@@ -0,0 +1,32 @@
+document.addEventListener(
+  "DOMContentLoaded", function(){
+    if ($("#token_submit") != null) { //token_submitというidがnullの場合、下記コードを実行しない
+      Payjp.setPublicKey("pk_test_e0182fd565758614f20265fb"); //ここに公開鍵を直書き
+      $("#token_submit").on("click", function(e){ //ボタンが押されたときに作動します
+        e.preventDefault(); //ボタンを一旦無効化します
+        let card = {
+          number: $("#card_number").val(),
+          cvc: $("#cvc").val(),
+          exp_month: $("#exp_month").val(),
+          exp_year: $("#exp_year").val()
+        }; //入力されたデータを取得します。
+        Payjp.createToken(card, function(status, response){
+          if (status === 200) { //成功した場合
+            $("#card_number").removeAttr("name");
+            $("#cvc").removeAttr("name");
+            $("#exp_month").removeAttr("name");
+            $("#exp_year").removeAttr("name"); //データを自サーバにpostしないように削除
+            $("#card_token").append(
+              $('<input type="hidden" name="payjp-token">').val(response.id)
+            ); //取得したトークンを送信できる状態にします
+            $("#charge-form").submit();
+            alert("登録が完了しました"); //確認用
+          } else {
+            alert("カード情報が正しくありません。"); //確認用
+          }
+        });
+      });
+    }
+  },
+  false
+);
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_login, except: [:index, :show, :destroy]
-  before_action :set_item, only: [:show, :edit, :destroy]
+  before_action :move_to_login, except: :index
 
   def index
     @parents = Category.where(ancestry: nil)
@@ -22,24 +21,7 @@ class ItemsController < ApplicationController
       redirect_to "/items/new", flash: { error: @item.errors.full_messages }
     end
   end
-
-  def destroy
-    unless @item.seller_id == current_user.id && @item.destroy
-      redirect_to  item_path(@item)
-    end
-  end
-
-  def show
-    @parents = Category.where(ancestry: nil)
-    @item_category_grandchild = Category.find(@item.category_id)
-    @item_category_child = @item_category_grandchild.parent
-    @item_category_parent = @item_category_child.parent
-  end
-
-  def move_to_login
-    redirect_to "/users/sign_in", notice: 'ログインするとご利用いただけます。' unless user_signed_in?
-  end
-
+  
   # 親カテゴリーが選択された後に動くアクション
   def get_category_children
     @category_children = Category.find(params[:parent_id]).children
@@ -51,15 +33,13 @@ class ItemsController < ApplicationController
   end
 
   private
-
-  def set_item
-    @item = Item.find(params[:id])
-  end
   
+  def move_to_login
+    redirect_to "/users/sign_in", notice: 'ログインするとご利用いただけます。' unless user_signed_in?
+  end
+
   def item_params
     params.require(:item).permit(:name, :brand, :explanation, :status, :sell_or_sold, :delivery_burden,
        :prefecture_id, :delivery_day, :price, :seller_id, :buyer_id, :category_id, images_attributes: [:image])
   end
-
-
 end
